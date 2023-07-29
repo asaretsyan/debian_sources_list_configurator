aec82ce8751b4114764964b136c3b98dd2578761
asaretsyan <136930997+asaretsyan@users.noreply.github.com>
2023-07-29 09:05:56 +0300

    Добавлен файл install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEBIAN_RELEASE=`cat /etc/*-release 2> /dev/null | grep PRETTY_NAME | awk -F "=" {'print $2'} | awk -F "(" {'print $2'} | awk -F ")" {'print $1'}`
+
+echo "" > /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian $DEBIAN_RELEASE main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian $DEBIAN_RELEASE main contrib non-free" >> /etc/apt/sources.list
+echo "" >> /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian-security/ $DEBIAN_RELEASE-security main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian-security/ $DEBIAN_RELEASE-security main contrib non-free" >> /etc/apt/sources.list
+echo "" >> /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian $DEBIAN_RELEASE-updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian $DEBIAN_RELEASE-updates main contrib non-free" >> /etc/apt/sources.list
+
+
+
+
+
+
+
+
